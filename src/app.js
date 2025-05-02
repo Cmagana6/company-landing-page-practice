ae631b4a189ed34935f48d72113eeb9086e0bdf4
CarlosFirstReference <carlos.magana10@gmail.com>
2025-05-01 20:29:44 -0600

    crud implemented

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const contentRoutes = require("./routes/contentRoutes");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //Middleware to parse JSON
 
 //Routes
-app.use("/api/content", contentRoutes);
+app.use("/api/v1/content", contentRoutes);
 
 module.exports = app;
